f1119ecda3fcba10512f74c9fcc24120f76cdf04
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-05-07 15:17:40 +0200

    Added Welcome exercise

--- Welcome.java
+++ Welcome.java
@@ -1,12 +1,22 @@
 //Inicializa la clase Welcome
 //Crea el método main
-
+public class Welcome {
+    public static void main(String[] args) {
 //Tarea 1
 //Define el método greeting que imprima en terminal ¡Hola FemCoder!
 //Ejecuta el método greeting 
-
+greeting();
 //Tarea 2
 //Define el método greetingCoder que reciba un nombre y que imprima en terminal ¡Hola <name>!
-//Ejecuta el método greetingCoder 
+//Ejecuta el método greetingCoder
+greetingCoder("Mariia");
+    }
+public static void greeting() {
+        System.out.println("¡Hola FemCoder!");
+}
+public static void greetingCoder(String name) {
+        System.out.println("¡Hola " + name + "!");
+    }
+}
 
 //¿Sabes donde definir el método y en dónde lo has de ejecutar? Si no lo sabes vuelve al archivo de Resources.md
